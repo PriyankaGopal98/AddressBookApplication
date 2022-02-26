00268c4522a6bf158ceafb3dab3163a383ed6bbb
priyankagopal <pgopal@horizon.csueastbay.edu>
2022-02-25 18:51:55 -0800

    Fix errors

--- AddressBookApplication/src/address/data/AddressBook.java
+++ AddressBookApplication/src/address/data/AddressBook.java
@@ -1,6 +1,5 @@
 package address.data;
 
-
 import java.util.ArrayList;
 import java.util.List;
 import java.io.IOException;
--- AddressBookApplication/src/address/data/AddressEntryTest.java
+++
@@ -1,7 +0,0 @@
-package address.data;
-import static org.junit.jupiter.api.Assertions.*;
-
-class AddressEntryTest {
-    AddressEntry addressEntry
-
-}
